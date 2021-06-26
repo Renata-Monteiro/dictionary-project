531b2c1a0037f43f6c8c3ecd62badeaeaa3f0e9a
Renata-Monteiro <renata_filipa_1996@hotmail.com>
2021-06-26 17:38:41 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">  
+      <h1>
+        SheCodes Dictionary
+        </h1>     
+      </header>
+        <div className="body">
+
+        </div>
+    </div>
+  );
+}
+
