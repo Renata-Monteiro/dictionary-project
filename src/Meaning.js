a186f54dfc708fa0bf06f46cc9700751802efa78
Renata-Monteiro <renata_filipa_1996@hotmail.com>
2021-07-11 10:57:52 +0100

    Created a new component to handle the Phonetics of each word

--- src/Meaning.js
+++ src/Meaning.js
@@ -2,8 +2,7 @@ import React from "react";
 import Synonyms from "./Synonyms";
 
 export default function Meaning(props){
-    console.log(props.meaning)
-    return(
+  	return(
         <div className="Meaning">     
         	<h4> {props.meaning.partOfSpeech}</h4>
 					{props.meaning.definitions.map(function(definition, index){
--- src/Results.js
+++ src/Results.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetic from "./Phonetic";
 
 export default function Results(props){
 	console.log(props.results);
@@ -9,6 +10,13 @@ export default function Results(props){
 				<h3>
 					{props.results.word}
 				</h3>
+				{props.results.phonetics.map(function(phonetic, index){
+					return (
+						<div key={index}>
+							<Phonetic phonetic={phonetic} />
+						</div>
+					)
+				})}
 				{props.results.meanings.map(function(meaning, index) {
 					return (
 						<div key={index}>
